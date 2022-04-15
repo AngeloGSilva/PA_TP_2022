97751cec2a690ecd1fd3087a593eafbbefa6330a
AngeloGSilva <47340682+AngeloGSilva@users.noreply.github.com>
2022-04-15 17:54:18 +0100

    #hash

--- src/pt/isec/pa/apoio_poe/model/data/Docente.java
+++ src/pt/isec/pa/apoio_poe/model/data/Docente.java
@@ -1,5 +1,7 @@
 package pt.isec.pa.apoio_poe.model.data;
 
+import java.util.Objects;
+
 public class Docente {
     String nome_Docente;
     String email_Docente;
@@ -9,5 +11,18 @@ public class Docente {
         this.nome_Docente = nome_Docente;
         this.email_Docente = email_Docente;
         this.papel_Docente = papel_Docente;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Docente docente = (Docente) o;
+        return papel_Docente == docente.papel_Docente && Objects.equals(nome_Docente, docente.nome_Docente) && Objects.equals(email_Docente, docente.email_Docente);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(nome_Docente, email_Docente, papel_Docente);
     }
 }
