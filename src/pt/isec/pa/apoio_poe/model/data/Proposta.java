f069cb7e89c5f49fae3ce3b70fb1de240c4639f4
AngeloGSilva <47340682+AngeloGSilva@users.noreply.github.com>
2022-04-15 17:55:38 +0100

    #hash

--- src/pt/isec/pa/apoio_poe/model/data/Proposta.java
+++ src/pt/isec/pa/apoio_poe/model/data/Proposta.java
@@ -1,5 +1,7 @@
 package pt.isec.pa.apoio_poe.model.data;
 
+import java.util.Objects;
+
 public class Proposta {
     String cod_ID = null;
     String titulo = null;
@@ -14,5 +16,18 @@ public class Proposta {
         this.codigo_Aluno = codigo_Aluno;
         this.codigo_Docente = codigo_Docente;
         this.ramo = ramo;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Proposta proposta = (Proposta) o;
+        return Objects.equals(cod_ID, proposta.cod_ID) && Objects.equals(titulo, proposta.titulo) && Objects.equals(codigo_Aluno, proposta.codigo_Aluno) && Objects.equals(codigo_Docente, proposta.codigo_Docente) && Objects.equals(ramo, proposta.ramo);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(cod_ID, titulo, codigo_Aluno, codigo_Docente, ramo);
     }
 }
