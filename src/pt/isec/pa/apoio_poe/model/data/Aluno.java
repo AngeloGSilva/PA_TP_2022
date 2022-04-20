aaaba072506e25124c517a137086e68cd2a5d9f1
AngeloGSilva <47340682+AngeloGSilva@users.noreply.github.com>
2022-04-20 22:17:29 +0100

    w

--- src/pt/isec/pa/apoio_poe/model/data/Aluno.java
+++ src/pt/isec/pa/apoio_poe/model/data/Aluno.java
@@ -57,7 +57,7 @@ public class Aluno {
     }
 
     @Override
-    public boolean equals(Object o) {gi
+    public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Aluno aluno = (Aluno) o;
