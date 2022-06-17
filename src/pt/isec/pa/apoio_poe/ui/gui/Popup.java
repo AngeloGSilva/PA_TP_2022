c2cb18b0de3141b050a94d2b86fe58adb6f877fa
AngeloGSilva <47340682+AngeloGSilva@users.noreply.github.com>
2022-06-17 23:55:31 +0100

    8

--- src/pt/isec/pa/apoio_poe/ui/gui/Popup.java
+++ src/pt/isec/pa/apoio_poe/ui/gui/Popup.java
@@ -3,6 +3,7 @@ package pt.isec.pa.apoio_poe.ui.gui;
 import javafx.geometry.Pos;
 import javafx.scene.*;
 import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.*;
 import javafx.stage.*;
 import pt.isec.pa.apoio_poe.model.ProgManager;
@@ -15,7 +16,32 @@ public class Popup extends VBox{
         popupwindow.initModality(Modality.APPLICATION_MODAL);
         Label lb = new Label();
         TableView<Aluno> tabelaAlunos = new TableView<>();
-        TableColumn nome_Aluno = new TableColumn<>(Nome);
+        TableColumn nome_Aluno = new TableColumn<>("Nome");
+        TableColumn numero_Aluno = new TableColumn<>("Numero");
+        TableColumn classificacao_Aluno = new TableColumn<>("Classificacao");
+        TableColumn curso_Aluno = new TableColumn<>("Curso");
+
+        nome_Aluno.setCellValueFactory(new PropertyValueFactory<Aluno, String>("nome_Aluno"));
+        numero_Aluno.setCellValueFactory(new PropertyValueFactory<Aluno, String>("nr_Aluno"));
+        classificacao_Aluno.setCellValueFactory(new PropertyValueFactory<Aluno, String>("classificacao_Aluno"));
+        curso_Aluno.setCellValueFactory(new PropertyValueFactory<Aluno, String>("curso"));
+
+        tabelaAlunos.getColumns().addAll(numero_Aluno,nome_Aluno,curso_Aluno,classificacao_Aluno);
+
+        VBox vBox = new VBox();
+        lb.setAlignment(Pos.CENTER);
+        vBox.setAlignment(Pos.CENTER);
+        vBox.getChildren().addAll(lb,tabelaAlunos);
+        Scene nscene = new Scene(vBox, 200, 100);
+        popupwindow.setMinWidth(250);
+        popupwindow.setMinHeight(150);
+
+        popupwindow.setMaxWidth(nscene.getWidth());
+        popupwindow.setMaxHeight(nscene.getHeight());
+
+        //popupwindow.initStyle(StageStyle.TRANSPARENT);
+        popupwindow.setScene(nscene);
+        popupwindow.showAndWait();
     }
 
 
